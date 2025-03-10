86dc6c003bb050167748694f65d6a2cc075702f6
carolinalima86 <carolclima86@gmail.com>
2025-03-10 19:59:00 -0300

    Aula03 Lab. Python

---
+++ Aula03.py
@@ -0,0 +1,18 @@
+   # Desafio
+# Criar um programa para verificar a Maior Idade
+
+
+# Passo 1: Criar uma variavel
+# Passo 2: Criar uma estrutura de condição para verificar a Idade
+# Passo 3: Exibir a frase adequada de acordo com a Idade da pessoa
+
+idade = int (input("Digite sua idade"))
+
+if idade >= 18:
+    print("Você é maior de idade")
+
+elif idade >=12:
+    print("Você é um adolescente")
+
+else:
+    print("Você é uma criança")
