75094c89848a3d95c6f5c6294a37db01ba9b0ea7
carolinalima86 <carolclima86@gmail.com>
2025-03-11 20:17:04 -0300

    Exercio Aula07 python

---
+++ Aula07.py
@@ -0,0 +1,15 @@
+def is_palindromo(texto):
+    texto_limpo = ''.join(char.lower()
+                          for char in texto 
+                          if char.isalnum())
+    return texto_limpo == texto_limpo[:: -1]
+frase = "A cara rajada da jararaca"
+resultado = is_palindromo(frase)
+
+if resultado == True:
+    Resposta = 'Sim'
+
+else:
+    Resposta = 'Não'
+
+    print(f"'{frase}' é um palindromo? {Resposta}")
