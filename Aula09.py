b496864740b533374124e253fb9d7da26404451d
carolinalima86 <carolclima86@gmail.com>
2025-03-27 20:38:51 -0300

    Atividade para obter cotação da moeda

---
+++ Aula09.py
@@ -0,0 +1,30 @@
+import requests 
+from datetime import datetime
+
+def obter_cotacao(moeda):
+    url = f"https://economia.awesomeapi.com.br/json/last/{moeda} -BRL"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        dados = response.json()
+        cotacao = dados[f"{moeda}BRL"]
+        return f"""
+        moeda: {moeda} para BRL
+        Valor: R${float(cotacao['bid']):.2f}
+        maxima: R${float(cotacao['high']):.2f}
+        minima; R${float(cotacao['low']):.2f}
+        data/hora: {datetime.fromtimestamp(int(cotacao['timestamp']))}
+        """
+    except requests.RequestException as e:
+        return f"Erro ao obter cotacao: {e}"
+    except KeyError:
+        return "Moeda não encontrada ou não suportada"
+    
+def main():
+    moeda = input ("Digite o valor da moeda para cotação(ex: USD,EUR,GBP): ").upper()
+    print("\nObtendo cotação...")    
+    resultado = obter_cotacao(moeda)
+    print(resultado)
+
+if __name__ == "__main__":
+    main()
